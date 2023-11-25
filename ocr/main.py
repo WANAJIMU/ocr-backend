3219dd1bccbbc383dcf5d89406477764f53c171c
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2023-11-25 12:45:54 +0300

    image loading

--- ocr/main.py
+++ ocr/main.py
@@ -2,3 +2,15 @@ import cv2
 import PIL from Image
 import pytesseract
 
+# open up an image
+im_file = 'data/cert1.jpg'
+im  = Image.open(im_file)
+
+# print(im)
+# print(im.size)
+
+# show image
+# im.show()
+
+# save image
+im.save('temp/cert.jpg')
